021d8bcab09fe5f6e0304d372b8727d075307137
BangkitSimmabur <bkt.simmabur@gmail.com>
2021-05-01 19:07:39 +0700

    update pages

--- lib/screens/tabs/article_tab/article_main.dart
+++ lib/screens/tabs/article_tab/article_main.dart
@@ -1,22 +1,141 @@
 import 'package:flutter/material.dart';
-import 'package:revisit/Screens/tabs/article_tab/create_artikel.dart';
+import 'package:provider/provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:revisit/components/revisit_spinner.dart';
 import 'package:revisit/constant.dart';
 import 'package:revisit/platform/platform_main.dart';
+import 'package:revisit/screens/tabs/article_tab/article_Card.dart';
+import 'package:revisit/screens/tabs/article_tab/create_artikel.dart';
+import 'package:revisit/service/article_service.dart';
+import 'package:revisit/service/auth_service.dart';
+import 'package:revisit/service/location_service.dart';
 
 class ArticleMain extends StatefulWidget {
   @override
-  _ArticleMainState createState() => _ArticleMainState();
+  State<StatefulWidget> createState() {
+    return ArticleMainState();
+  }
 }
 
-class _ArticleMainState extends State<ArticleMain> {
+class ArticleMainState extends State<ArticleMain> {
+  AuthService authService;
+  ArticleService articleService;
+  LocationService locationService;
+  var currentPage = 1;
+  var currentSearchPage = 1;
+  final focusNode = FocusNode();
+  final searchController = TextEditingController();
+  var isLoading = true;
+  var canScroll = true;
+  var canSearchProductScroll = true;
+  var canSearchStoreScroll = true;
+  var currentSumArticle = 0;
+  var currentSumSearchProducts = 0;
+  var currentSumSearchStores = 0;
+  var currentKeyword;
+  var searchMode = false;
+  var isShowSearch = false;
+  bool isBottomLoading = false;
+
+  final refreshController = RefreshController(
+    initialRefresh: false,
+    initialLoadStatus: LoadStatus.canLoading,
+  );
+  final scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    searchController.dispose();
+    scrollController.dispose();
+    refreshController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, initDataState);
+
+    // searchController.addListener(_onChangeSearch);
+
+    super.initState();
+  }
+
+  Future initDataState({
+    bool isMainLoading = true,
+  }) async {
+    if (!isMainLoading && !canScroll) return;
+
+    if (isMainLoading) setState(() => isLoading = true);
+
+    await articleService.getAllArticle(
+        currentPage,
+        true
+    );
+
+    // if (storyService.currentKeyWord != null &&
+    //     storyService.currentKeyWord.isNotEmpty) {
+    //   setState(() {
+    //     searchMode = true;
+    //     isShowSearch = true;
+    //   });
+    //
+    //   if (storyService.currentKeyWord != null &&
+    //       storyService.currentKeyWord.isNotEmpty) {
+    //     setState(
+    //           () => searchController.text = storyService.currentKeyWord,
+    //     );
+    //   }
+    //
+    //   await storeService.searchGetAllStore(
+    //     keyword: searchController.text,
+    //     catIds: storeService.storeCategories,
+    //     tag: storeService.selectedTag,
+    //     byLocation: storeService.searchByLocation,
+    //     pagination: storeSearchPagination,
+    //   );
+    //
+    //   await menuItemService.searchGetAllProduct(
+    //     keyword: searchController.text,
+    //     catIds: storeService.storeCategories,
+    //     tag: storeService.selectedTag,
+    //     byLocation: storeService.searchByLocation,
+    //     pagination: productSearchPagination,
+    //   );
+    // }
+
+    if (isMainLoading) setState(() => isLoading = false);
+
+    if (currentSumArticle >= articleService.articleExplore.length) {
+      return setState(() => canScroll = false);
+    }
+
+    setState(
+          () => currentSumArticle = articleService.articleExplore.length,
+    );
+
+    print(canScroll);
+    print(currentSumArticle);
+
+    return;
+  }
+
+  @override
+  void setState(fn) {
+    if (!mounted) return;
+
+    super.setState(fn);
+  }
+
   @override
   Widget build(BuildContext context) {
+    articleService = Provider.of<ArticleService>(context);
+    authService = Provider.of<AuthService>(context);
+    locationService = Provider.of<LocationService>(context);
+
     return Scaffold(
-      backgroundColor: Colors.red,
       body: Container(
-        child: Text(
-          "Article"
-        ),
+        child: _childElement,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -28,7 +147,149 @@ class _ArticleMainState extends State<ArticleMain> {
     );
   }
 
+  Widget get _childElement {
+    return SmartRefresher(
+      controller: refreshController,
+      enablePullDown: true,
+      enablePullUp: true,
+      onRefresh: _onRefresh,
+      onLoading: _onCheckEndScroll,
+      footer: ClassicFooter(
+        idleText: 'Tarik ke atas untuk memuat lebih banyak',
+        idleIcon: Icon(
+          Icons.keyboard_arrow_up,
+        ),
+        loadingText: 'Memuat data',
+        noDataText: 'Tidak ada data',
+        canLoadingText: 'Lepaskan untuk memuat lebih banyak',
+        failedText: 'Gagal memuat data',
+      ),
+      child: ListView(
+        controller: scrollController,
+        children: [
+          ArticleCard(this),
+          isBottomLoading ? RevisitSpinner() : Container(),
+        ],
+      ),
+    );
+  }
+
   void _toCreateArticle() {
     MainPlatform.transitionToPage(context, CreateArticle());
   }
+
+  void _onRefresh() async {
+    // if (isShowSearch) {
+    //   return onSearch();
+    // }
+
+    setState(() {
+      currentPage = 1;
+      // storeSearchPagination.resetPagination();
+      // productSearchPagination.resetPagination();
+      currentSumArticle = 0;
+      currentSumSearchProducts = 0;
+      canScroll = true;
+    });
+
+    await initDataState();
+
+    return refreshController.refreshCompleted(
+      resetFooterState: true,
+    );
+  }
+
+  void _onCheckEndScroll() async {
+    setState(() {
+      isBottomLoading = true;
+    });
+    // if (isShowSearch) {
+    //   if (selectedTab == 'tab1') {
+    //     setState(() => productSearchPagination.doIncrement());
+    //     if (!canSearchProductScroll) {
+    //       return refreshController.loadNoData();
+    //     }
+    //
+    //     await menuItemService.searchGetAllProduct(
+    //       keyword: searchController.text,
+    //       catIds: storeService.storeCategories,
+    //       tag: storeService.selectedTag,
+    //       byLocation: storeService.searchByLocation,
+    //       pagination: productSearchPagination,
+    //     );
+    //
+    //     if (currentSumSearchProducts >=
+    //         menuItemService.productSearchResult.length) {
+    //       setState(() => canSearchProductScroll = false);
+    //     }
+    //
+    //     setState(
+    //           () => currentSumSearchProducts =
+    //           menuItemService.productSearchResult.length,
+    //     );
+    //
+    //     if (canSearchProductScroll) {
+    //       return refreshController.loadComplete();
+    //     }
+    //
+    //     return refreshController.loadNoData();
+    //   }
+    //
+    //   setState(() => storeSearchPagination.doIncrement());
+    //   if (!canSearchStoreScroll) {
+    //     return refreshController.loadNoData();
+    //   }
+    //
+    //   await storeService.searchGetAllStore(
+    //     keyword: searchController.text,
+    //     catIds: storeService.storeCategories,
+    //     tag: storeService.selectedTag,
+    //     byLocation: storeService.searchByLocation,
+    //     pagination: storeSearchPagination,
+    //   );
+    //
+    //   if (currentSumSearchStores >= storeService.storeSearchResult.length) {
+    //     setState(() => canSearchStoreScroll = false);
+    //   }
+    //
+    //   setState(
+    //         () => currentSumSearchStores = storeService.storeSearchResult.length,
+    //   );
+    //
+    //   if (canSearchStoreScroll) {
+    //     return refreshController.loadComplete();
+    //   }
+    //
+    //   return refreshController.loadNoData();
+    // }
+
+    setState(() => currentPage++);
+    if (!canScroll) {
+      return refreshController.loadNoData();
+    }
+
+    await articleService.getAllArticle(
+      currentPage,
+      false,
+    );
+
+    if (currentSumArticle >= articleService.articleExplore.length) {
+      setState(() => canScroll = false);
+    }
+
+    setState(
+          () => currentSumArticle = articleService.articleExplore.length,
+    );
+
+    setState(() {
+      isBottomLoading = false;
+    });
+
+    if (canScroll) {
+      return refreshController.loadComplete();
+    }
+
+    return refreshController.loadNoData();
+  }
+
 }
--- lib/screens/tabs/article_tab/create_artikel.dart
+++ lib/screens/tabs/article_tab/create_artikel.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:revisit/components/button_full.dart';
 import 'package:revisit/components/common_appbar.dart';
+import 'package:revisit/components/image_input_common.dart';
+import 'package:revisit/components/inputCommon.dart';
+import 'package:revisit/components/input_map.dart';
 import 'package:revisit/constant.dart';
+import 'package:revisit/models/location.dart';
+import 'package:revisit/platform/platform_main.dart';
+import 'package:revisit/service/article_service.dart';
 
 class CreateArticle extends StatefulWidget {
   @override
@@ -8,16 +18,256 @@ class CreateArticle extends StatefulWidget {
 }
 
 class _CreateArticleState extends State<CreateArticle> {
+  File _imageFile;
+  bool _isLoading = false;
+  bool _isLoadingImg = false;
+  LocationData location;
+
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _descController = TextEditingController();
+
+  ArticleService _articleService;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    location = new LocationData(null, null, "");
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _articleService = Provider.of<ArticleService>(context);
+
     return Scaffold(
       appBar: _appBar,
+      body: _childElement,
+    );
+  }
+
+  Widget get _childElement {
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: _formElement,
+              ),
+            ),
+            _createStoreBtn,
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget get _formElement {
+    return ListView(
+      padding: EdgeInsets.all(Constant.DEFAULT_PADDING_VIEW),
+      children: [
+        Container(
+          height: Constant.MINIMUM_SPACING,
+        ),
+        RevisitInputCommon(
+          'Judul Artikel',
+          hintText: 'Judul Artikel',
+          labelMinLine: 1,
+          labelMaxLine: 3,
+          isDense: true,
+          inputController: _titleController,
+          noLabel: true,
+          labelColor: Colors.black,
+        ),
+        Container(
+          height: Constant.MINIMUM_SPACING_LG,
+        ),
+        _photoIntput,
+        Container(
+          height: Constant.MINIMUM_SPACING_LG,
+        ),
+        _locationElement,
+        Container(
+          height: Constant.MINIMUM_SPACING_XLG,
+        ),
+        RevisitInputCommon(
+          'Deskripsi',
+          hintText: 'Deskripsi',
+          labelMinLine: 3,
+          labelMaxLine: 3,
+          isDense: true,
+          inputController: _descController,
+          noLabel: true,
+          labelColor: Colors.black,
+        ),
+      ],
+    );
+  }
+
+  Widget get _locationElement {
+    var isLocationExist = location.address.isNotEmpty;
+    TextStyle _style = TextStyle(
+      fontSize: Constant.MINIMUM_FONT_SIZE - 2,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          height: Constant.MINIMUM_SPACING * 2,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: 50),
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                  vertical: Constant.MINIMUM_SPACING, horizontal: 8),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.zero,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: isLocationExist
+                          ? Text(
+                        location.address,
+                        style: _style,
+                      )
+                          : Text(
+                        'Masukkan Lokasi',
+                        style: _style,
+                      )),
+                  Container(
+                    width: Constant.MINIMUM_SPACING,
+                  ),
+                  GestureDetector(
+                    onTap: _onNavigateInputMap,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Constant.GRAY_TEXT),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 2, vertical: 1),
+                      child: Icon(
+                        Icons.place,
+                        color: Constant.GRAY_TEXT3,
+                        size: Constant.MINIMUM_FONT_SIZE + 6,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future _onNavigateInputMap() async {
+    var data = await MainPlatform.transitionToPage(
+      context,
+      RevisitInputMap(
+        initLocationData: location,
+      ),
+    );
+
+    print(data);
+
+    if (data == null) return;
+
+    setState(() => location = data);
+  }
+
+  Widget get _createStoreBtn {
+    return RevisitButtonFull(
+      "BUAT CERITA",
+      buttonColor: Constant.blue01,
+      onClick: _onCreateArticle,
+      isLoading: _isLoading,
+      padding: Constant.MINIMUM_PADDING_BUTTON_MD,
+      labelSize: Constant.MINIMUM_FONT_SIZE - 2,
+    );
+  }
+
+  void _onCreateArticle() async {
+    if (_titleController.text.isEmpty || _titleController.text == null) {
+      MainPlatform.showFloatingSnackbar(
+        context,
+        'Judul artikel tidak diisi',
+        color: Constant.red01,
+      );
+      return;
+    }
+
+    if (_descController.text.isEmpty || _descController.text == null) {
+      MainPlatform.showFloatingSnackbar(
+        context,
+        'Isi artikel tidak diisi',
+        color: Constant.red01,
+      );
+      return;
+    }
+    if (location.address == null || location.address.isEmpty) {
+      MainPlatform.showFloatingSnackbar(
+        context,
+        'Lokasi artikel tidak diisi',
+        color: Constant.red01,
+      );
+      return;
+    }
+    setState(() {
+      _isLoading = true;
+    });
+    MainPlatform.showLoadingAlert(context, 'Membuat artikel');
+    var serverLog = await _articleService.createArticle(
+      title: _titleController.text,
+      action: ItemAction.create,
+      locationData: location,
+      photo: _imageFile,
+      text: _descController.text,
+    );
+    setState(() {
+      _isLoading = false;
+    });
+
+    if(serverLog.success) {
+      MainPlatform.backTransitionPage(context);
+      MainPlatform.backTransitionPage(context);
+    }
+    if(!serverLog.success) {
+      MainPlatform.backTransitionPage(context);
+      MainPlatform.showErrorSnackbar(context, 'Gagal membuat artikel');
+    }
+  }
+
+  Widget get _photoIntput {
+    return RevisitInputImageCommon(
+      null,
+      onSavedImage: (File img) {
+        _imageFile = img;
+      },
+      isLoading: _isLoadingImg,
     );
   }
 
   Widget get _appBar {
     return RevisitAppbar(
-      'Registrasi',
+      'Buat Artikel',
       bgColor: Constant.blue01,
       trailingColor: Colors.white,
     );
